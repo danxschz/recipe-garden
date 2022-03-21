80b7c2388f56ecfd57310b63a1ba1e5a91981ac2
Daniel Sanchez <danxschz@gmail.com>
2022-03-21 00:08:00 -0400

    Add recipe app views
    - Set five views with their corresponding urls and templates:
      * RecipeList
      * RecipeDetail
      * IngredientList
      * RecipesByIngredient
      * RecipesByCategory

---
+++ recipes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'recipes'
+urlpatterns = [
+    path('recipes/', views.RecipeList.as_view(), name='recipe_list'),
+    path('ingredients/', views.IngredientList.as_view(), name='ingredient_list'),
+    path('recipe/<int:pk>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    path('recipes/ingredient/<int:pk>/', views.RecipesByIngredient.as_view(), name='recipes_by_ingredient'),
+    path('recipes/category/<int:pk>/', views.RecipesByCategory.as_view(), name='recipes_by_category'),
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
+from django.urls import reverse, reverse_lazy
+from django.views import View
+from .models import Recipe, Ingredient, RecipeIngredient, Category
 
-# Create your views here.
+class RecipeList(ListView):
+    model = Recipe
+    template_name = 'recipes/recipe_list.html'
+    context_object_name = 'recipe_list'
+
+class RecipeDetail(DetailView):
+    model = Recipe
+    template_name = 'recipes/recipe_detail.html'
+    context_object_name = 'recipe'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Ingredients
+        ingredients = RecipeIngredient.objects.filter(recipe=self.object)
+        context['ingredients'] = ingredients
+
+        # Directions
+        directions = self.object.directions.split('\n')
+        context['directions'] = directions
+
+        # Similar recipes
+        similar_recipes = Recipe.objects.filter(category=self.object.category)
+        similar_recipes = similar_recipes.exclude(name=self.object.name)[0:4]
+        context['similar_recipes'] = similar_recipes
+
+        return context
+
+class IngredientList(ListView):
+    model = Ingredient
+    template_name = 'recipes/ingredient_list.html'
+    queryset = Ingredient.objects.order_by('name')
+    context_object_name = 'ingredient_list'
+
+class RecipesByIngredient(View):
+    template_name = 'recipes/recipes_by_ingredient.html'
+
+    def get(self, request, pk):
+        ingredient = Ingredient.objects.get(pk=pk)
+        queryset = RecipeIngredient.objects.filter(ingredient=ingredient)
+        context = {'recipe_list': queryset, 'ingredient': ingredient}
+        return render(request, self.template_name, context)
+
+class RecipesByCategory(View):
+    template_name = 'recipes/recipes_by_category.html'
+
+    def get(self, request, pk):
+        c = Category.objects.get(pk=pk)
+        queryset = Recipe.objects.filter(category=c)
+        context = {'recipe_list': queryset}
+        return render(request, self.template_name, context)
+
+
